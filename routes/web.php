d438fb085cb899c0d6e48e4635ea3c5cc6907194
MIkhsanB546 <mikhsanb546@gmail.com>
2024-01-11 10:17:12 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LuasPersegiController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/luas-persegi', function () {
+    return view('luas_persegi_form');
+});
+
+Route::post('/luas-persegi', [LuasPersegiController::class, 'hitungLuas']);
